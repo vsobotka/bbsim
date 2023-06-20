f66a57dec50535e2e8b24dd9e3fea7b2a51542a1
Václav Sobotka <sobotka.vaclav@gmail.com>
2023-06-20 21:37:43 +0200

    Add pyodide to the website

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+
+declare const loadPyodide: any;
+
+async function hello_python() {
+  const pyodide = await loadPyodide();
+  return await pyodide.runPythonAsync("1+3");
+}
+
 export function App() {
-  return <h1>BBSim</h1>;
+  const [output, setOutput] = useState("(loading...)");
+
+  useEffect(() => {
+    const run = async () => {
+      const out = await hello_python();
+      setOutput(out);
+    };
+    run()
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <h1>{output}</h1>;
 }
