e09f67c55e20c2b4d838b36c396c764fce3785dc
Václav Sobotka <sobotka.vaclav@gmail.com>
2023-06-30 10:23:49 +0200

    Add Trials, make sure all labels reflect python variable name for troubleshooting

--- src/App.tsx
+++ src/App.tsx
@@ -9,26 +9,34 @@ export function App() {
   const [Def_HP, setDef_HP] = useState(100);
   const [Def_Helmet, setDef_Helmet] = useState(120);
   const [Def_Armor, setDef_Armor] = useState(95);
+  const [Trials, setTrials] = useState(10000);
 
   return (
     <>
       <TextInput
-        style={{ width: 100, display: "inline-block" }}
-        label="Defender HP"
+        style={{ width: 120 }}
+        label="Trials"
+        description="Number of trials to run"
+        onChange={(e) => setTrials(Number(e.currentTarget.value))}
+        value={Trials}
+      />
+      <TextInput
+        style={{ width: 120, display: "inline-block" }}
+        label="Def_HP"
         description="Defender's HP"
         onChange={(e) => setDef_HP(Number(e.currentTarget.value))}
         value={Def_HP}
       />
       <TextInput
-        style={{ width: 100, display: "inline-block" }}
-        label="Defender Helmet"
+        style={{ width: 120, display: "inline-block" }}
+        label="Def_Helmet"
         description="Defender's Helmet"
         onChange={(e) => setDef_Helmet(Number(e.currentTarget.value))}
         value={Def_Helmet}
       />
       <TextInput
-        style={{ width: 100, display: "inline-block" }}
-        label="Defender Armor"
+        style={{ width: 120, display: "inline-block" }}
+        label="Def_Armor"
         description="Defender's Armor"
         onChange={(e) => setDef_Armor(Number(e.currentTarget.value))}
         value={Def_Armor}
@@ -42,7 +50,7 @@ export function App() {
             (addition) => {
               setOutput((output) => [...output, addition]);
             },
-            { Def_HP, Def_Helmet, Def_Armor }
+            { Trials, Def_HP, Def_Helmet, Def_Armor }
           )
             .catch((e) => setError(e.toString()))
             .finally(() => setRunning(false));
