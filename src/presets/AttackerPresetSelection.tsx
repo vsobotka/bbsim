cdb31e29b732096f2081d3213f84c592fb16af49
Václav Sobotka <sobotka.vaclav@gmail.com>
2023-08-02 14:33:46 +0200

    Make attacker preset selection pretty and responsive

--- src/presets/AttackerPresetSelection.tsx
+++ src/presets/AttackerPresetSelection.tsx
@@ -1,5 +1,5 @@
 import { attacker, Factions } from "./attacker.ts";
-import { Button } from "@mantine/core";
+import { Button, SimpleGrid, Title } from "@mantine/core";
 
 type Props = {
   selectedPreset: string | null;
@@ -10,31 +10,47 @@ export function AttackerPresetSelection({
   selectedPreset,
   setSelectedPreset,
 }: Props) {
-  return Object.keys(Factions).map((faction) => {
-    return (
-      <div>
-        <div>{faction}</div>
-        {attacker
-          .filter(
-            (attacker) =>
-              attacker.faction === Factions[faction as keyof typeof Factions]
-          )
-          .map((attacker) => {
-            return (
-              <Button
-                style={{ display: "block" }}
-                variant={selectedPreset === attacker.id ? "filled" : "subtle"}
-                onClick={() => {
-                  selectedPreset === attacker.id
-                    ? setSelectedPreset(null)
-                    : setSelectedPreset(attacker.id);
-                }}
-              >
-                {attacker.name}
-              </Button>
-            );
-          })}
-      </div>
-    );
-  });
+  return (
+    <SimpleGrid
+      cols={3}
+      spacing="xs"
+      verticalSpacing="sm"
+      breakpoints={[
+        { maxWidth: "xs", cols: 1 },
+        { maxWidth: "sm", cols: 2 },
+      ]}
+    >
+      {Object.keys(Factions).map((faction) => {
+        return (
+          <div key={faction} style={{ width: 250 }}>
+            <Title order={5}>{faction}</Title>
+            {attacker
+              .filter(
+                (attacker) =>
+                  attacker.faction ===
+                  Factions[faction as keyof typeof Factions]
+              )
+              .map((attacker) => {
+                return (
+                  <Button
+                    key={attacker.id}
+                    style={{ display: "block" }}
+                    variant={
+                      selectedPreset === attacker.id ? "filled" : "subtle"
+                    }
+                    onClick={() => {
+                      selectedPreset === attacker.id
+                        ? setSelectedPreset(null)
+                        : setSelectedPreset(attacker.id);
+                    }}
+                  >
+                    {attacker.name}
+                  </Button>
+                );
+              })}
+          </div>
+        );
+      })}
+    </SimpleGrid>
+  );
 }
